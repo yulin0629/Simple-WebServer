c7f6ba23d8d34b4d204a78f03dd9063c23beaca9
Wang Yulin <yulin0629@gmail.com>
2019-03-31 15:57:03 +0800

    Add get address info

--- main.c
+++ main.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+
+char portNumber[] = "80";
 
 int main(void) {
     
+    struct addrinfo *pRes, hints;
+    int err;
+
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_socktype = SOCK_STREAM;
+
+    err = getaddrinfo(NULL, portNumber, &hints, &pRes);
+
+    if (err != 0) {
+        printf("getaddrinfo : %s\n", gai_strerror(err));
+        return 1;
+    }
+
+    printf("AF_UNSPEC %d\n", AF_UNSPEC);
+    printf("AF_INET %d\n", AF_INET);
+    printf("hints.ai_family %d \n", hints.ai_family);
+    
+    struct addrinfo *pAddrInfo = pRes;
+    do
+    {
+        printf("pRes->ai_family %d \n", pAddrInfo->ai_family);
+    } while (pAddrInfo = pAddrInfo->ai_next);
+    
+    
+
+    // Open socket
     int mainSocket;
 
-    mainSocket = socket(AF_INET, SOCK_STREAM, 0);
+    mainSocket = socket(pRes->ai_family, pRes->ai_socktype, 0);
 
     if (mainSocket < 0) {
         printf("Open main socket failed\n");
